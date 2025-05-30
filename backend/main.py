afd146f0e6aab994738e167f0731cd12ab83de95
muramasa <muramasa.kyojin18@outlook.jp>
2025-05-30 17:32:43 +0900

    バックエンド、Fast APIの準備完了

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Viteのデフォルトポート
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
